ccf5a3323b07620ed9f830fbd0f76e78c12f666b
Josh Wood <wood2jp2@gmail.com>
2017-08-06 18:00:28 -0400

    updated readme, added comments, made new file for bonus

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bamazon
 Using MySQL, I will be tracking the inventory / orders of a store similar to Amazon.
+
+bamazonCustomer -
+
+This portion was relatively familiar as I had already done some SQL database integration / data manipulation for my Flash Card assignment last week. In this assignment, the customer is prompted the available items I have populated in my bamazon_DB. Using the inquirer package, they are able to choose an item based off their item ID, as well as a quantity. IF there is enough stock to complete the purchase, the customer's total in dollars is shown, and the quantity in SQL is updated. If not, the customer is notified of the shortage in stock, and is shown how many of that item are actually available.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+// node modules
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+// database connection
 const connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
@@ -9,16 +11,19 @@ const connection = mysql.createConnection({
   port: 3306
 });
 
-var bamazon = {
+// object and methods for customer, for readability
+var bamazonCustomer = {
 
+  // create connection to database using connect method
   createConnection: function() {
     connection.connect(function(err) {
       if (err) throw (err);
       console.log('Connected to bamazon_DB!');
-      bamazon.displayProducts();
+      bamazonCustomer.displayProducts();
     });
   },
 
+  // this loops through and shows the items available for purchase from products table
   displayProducts: function() {
     connection.query('SELECT * from products', function(err, res) {
       if (err) throw (err);
@@ -27,11 +32,11 @@ var bamazon = {
         console.log(res[i].item_id + ' - ' + res[i].product_name +
           ". Price: $" + res[i].price)
       };
-      bamazon.firstPrompt();
+      bamazonCustomer.firstPrompt();
     });
-
   },
 
+  // prompts the customer using inquirer, who can choose by ID based on products displayed
   firstPrompt: function() {
     inquirer.prompt([{
       name: 'itemID',
@@ -40,28 +45,33 @@ var bamazon = {
       name: 'quantity',
       message: 'How many would you like to buy?'
     }]).then(function(answers) {
+
+      // assigned variables for each answer, and turned the quantity into integer for future calculation
       var itemID = answers.itemID;
       var requestedQty = parseInt(answers.quantity);
 
+      // accessing specific chosen item's data from DB
       connection.query(`SELECT * from products WHERE item_id = ${itemID}`,
         function(err, res) {
+
+          // assigning variable to new quantity based on available stock and user request
           var newQuantity = parseInt(res[0].stock_quantity) - requestedQty;
-          console.log(requestedQty);
-          console.log(res[0].stock_quantity);
-          console.log(newQuantity);
+
+          // checks to see if there is enough stock to complete order
           if (newQuantity >= 0) {
-            console.log("Item purchased!");
+            console.log(`Item purchased! Your total is: $${res[0].price * requestedQty}`);
+
+            // if enough stock, updates quantity for item in database
             connection.query(`UPDATE products SET stock_quantity = ${newQuantity} WHERE item_id = ${itemID}`, function(err, res) {
               if (err) throw (err);
               console.log('Item quantity updated!')
             })
           } else {
-            console.log('Insufficient quantity!')
+            console.log(`Insufficient quantity! There are only ${res[0].stock_quantity} currently in stock and available to purchase!`)
           }
         })
-
     })
   }
 };
 
-bamazon.createConnection();
+bamazonCustomer.createConnection();
