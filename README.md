3c86a302752e039f305fc7cdb760f16c44e23646
Josh Wood <wood2jp2@gmail.com>
2017-08-07 23:02:43 -0400

    added the 'add new dept' functionality to supervisor file

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ This portion was relatively familiar as I had already done some SQL database int
 bamazonManager -
 
 This was set up similar to bamazonCustomer. The difference was that the manager had authority to do four commands: view all products, view products that needed restocking (less than 5 in stock), add to any product's inventory, or add a new product. A switch statement was much easier than doing if statements. Utilizing that, I was able to throw an action based off the manager's answer. The first two options were simply displaying the data for all products / products that were in low quantity (using a 'WHERE stock_quantity <=5' statement). Adding stock required me to first pull down the item in question from the database, then taking the manager's new quantity and adding that to that total, then updating the value in SQL, which required some research on my end. Lastly, adding a new product in wasn't too difficult, just a 'INSERT INTO' statement with some template literals, which are now my new favorite thing for strings (way easier than all those quotes and plus signs)
+
+bamazonSupervisor -
+
+The tough part here was figuring out how to join the two tables (departments and products) based off my very limited SQL experience. The 'Add New Department' functionality acted the same as adding a new product in the above iterations.
--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -30,7 +30,6 @@ var bamazonSupervisor = {
       type: 'list',
       choices: ['View Product Sales by Department', 'Create New Department']
     }]).then(function(answers) {
-      console.log('asdf');
       switch (answers.action) {
         case 'View Product Sales by Department':
           connection.query("SELECT departments.department_id, departments.department_name, departments.over_head_costs, products.product_sales, SUM(products.product_sales - departments.over_head_costs) AS total_profit FROM products LEFT JOIN departments ON departments.department_name=products.department_name GROUP BY departments.department_id, departments.department_name, departments.over_head_costs, products.product_sales", function(err, res) {
@@ -40,7 +39,22 @@ var bamazonSupervisor = {
             };
           });
           break;
-      }
+        case 'Create New Department':
+          inquirer.prompt([{
+            name: 'newDeptName',
+            message: 'Please enter the new department name.',
+          }, {
+            name: 'newDeptOverhead',
+            message: 'Please enter the overhead costs for this department.'
+          }]).then(function(answers) {
+            var newDeptName = answers.newDeptName;
+            var newOverhead = parseInt(answers.newDeptOverhead);
+            connection.query(`INSERT INTO departments (department_name, over_head_costs) VALUES ('${newDeptName}', ${newOverhead})`, function(err, res) {
+              console.log('Department has been added!');
+            });
+          });
+          break;
+      };
     });
   }
 };
